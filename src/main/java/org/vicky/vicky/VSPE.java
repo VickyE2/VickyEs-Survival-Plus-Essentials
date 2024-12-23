3aa450878a9436b7d549f87f5f08409dda9cf9d1
VickyE2 <f.b.cgamingco@gmail.com>
2024-12-23 18:32:22 +0100

    Initial commit.... D:

---
+++ src/main/java/org/vicky/vicky/VSPE.java
@@ -0,0 +1,17 @@
+package org.vicky.vicky;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VSPE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
