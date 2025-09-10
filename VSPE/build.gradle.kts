d79966b8da0b34736d11c7c5cf53eb83090d3e29
VickyE2 <f.b.cgamingco@gmail.com>
2025-09-10 17:13:23 +0100

    Awaiting fix.

--- VSPE/build.gradle.kts
+++ VSPE/build.gradle.kts
@@ -4,7 +4,9 @@ subprojects {
 
     dependencies {
         implementation("org.reflections:reflections:0.10.2")
-        api("io.github.vickye2:vspe-core:0.0.1-ARI")
+        api("io.github.vickye2:vspe-core:0.0.1-ARI") {
+            exclude(group = "io.github.vickye2", module = "vspe-core")
+        }
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
         testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0")
         testImplementation("org.jetbrains.kotlin:kotlin-test:2.1.10")
